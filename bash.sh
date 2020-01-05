ac5ffa936de5a622a8de12cafdaf02c907b92c9a
devansh-dvj <devansh.dvj@gmail.com>
2020-01-05 12:34:59 +0530

    05012020 1235

--- bash.sh
+++ bash.sh
@@ -10,45 +10,13 @@ echo "[alias]
   br = branch
   hist = log --pretty=format:'%h %ad | %s%d [%an]' --graph --date=short" >> ~/.gitconfig
 
-#WPS-OFFICE (WORD, EXCEL, PRESENTATION)
-sudo snap install wps-office
-
-#CHROME (BROWSER)
-#sudo echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list
-#wget https://dl.google.com/linux/linux_signing_key.pub
-#sudo apt-key add linux_signing_key.pub
-#sudo apt update
-#sudo apt install google-chrome-stable
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -O google-chrome-stable_current_amd64.deb
-sudo dpkg -i google-chrome-stable_current_amd64.deb
-sudo apt upgrade
-
-#OPERA (BROWSER)
-wget -qO- https://deb.opera.com/archive.key | sudo apt-key add -
-sudo add-apt-repository "deb [arch=i386,amd64] https://deb.opera.com/opera-stable/ stable non-free"
+#UNITY TWEAK TOOL
 sudo apt update
-sudo apt install opera-stable
+sudo apt install unity-tweak-tool gnome-tweak-tool
 
-#PDFSAM (PDF EDITOR)
-sudo apt install openjdk-8-jre libopenjfx-jni libopenjfx-java openjfx
-sudo echo "JAVA_HOME=\"/usr/lib/jvm/java-8-openjdk-amd64\"" >> /etc/environment
-source /etc/environment
-echo $JAVA_HOME
-wget https://github.com/torakiki/pdfsam/releases/download/v3.1.0.RELEASE/pdfsam-3.1.0.RELEASE-1_all.deb -O pdfsam-3.1.0.RELEASE-1_all.deb
-sudo dpkg -i pdfsam-3.1.0.RELEASE-1_all.deb
-
-#TERMINATOR
+#COMPIZCONFIG SETTINGS MANAGER
 sudo apt update
-sudo apt install terminator
-
-#NODEJS
-sudo apt install curl
-curl -sL https://deb.nodesource.com/setup_13.x | sudo -E bash -
-sudo apt install nodejs
-#ANGULAR-TS
-sudo npm install -g @angular/cli
-#BROWSER-SYNC
-sudo npm install -g browser-sync
+sudo apt install compizconfig-settings-manager
 
 #VIM
 sudo apt remove --assume-yes vim-tiny
@@ -70,23 +38,34 @@ nnoremap rm :nohlsearch<CR>
 highlight ExtraWhitespace ctermbg=red guibg=red
 match ExtraWhitespace /\s\+$/" > ./vimrc
 
-#FOXIT-READER
-#cd /tmp
-#wget http://cdn01.foxitsoftware.com/pub/foxit/reader/desktop/linux/2.x/2.4/en_us/FoxitReader2.4.1.0609_Server_x64_enu_Setup.run.tar.gz
-#tar -xvf FoxitReader*.tar.gz
-#sudo ./FoxitReader.enu.setup.2.4.1.0609\(r08f07f8\).x64.run
-wget http://cdn01.foxitsoftware.com/pub/foxit/reader/desktop/linux/2.x/2.1/en_us/FoxitReader2.1.0805_Server_x64_enu_Setup.run.tar.gz
-tar xzvf FoxitReader*.tar.gz
-sudo chmod a+x FoxitReader*.run
-sudo ./FoxitReader.*.run
-
-#UNITY TWEAK TOOL
+#TERMINATOR
 sudo apt update
-sudo apt install unity-tweak-tool gnome-tweak-tool
+sudo apt install terminator
 
-#COMPIZCONFIG SETTINGS MANAGER
+#CHROME (BROWSER)
+#sudo echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list
+#wget https://dl.google.com/linux/linux_signing_key.pub
+#sudo apt-key add linux_signing_key.pub
+#sudo apt update
+#sudo apt install google-chrome-stable
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -O google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb
+sudo apt upgrade
+
+#OPERA (BROWSER)
+wget -qO- https://deb.opera.com/archive.key | sudo apt-key add -
+sudo add-apt-repository "deb [arch=i386,amd64] https://deb.opera.com/opera-stable/ stable non-free"
 sudo apt update
-sudo apt install compizconfig-settings-manager
+sudo apt install opera-stable
+
+#NODEJS
+sudo apt install curl
+curl -sL https://deb.nodesource.com/setup_13.x | sudo -E bash -
+sudo apt install nodejs
+#ANGULAR-TS
+sudo npm install -g @angular/cli
+#BROWSER-SYNC
+sudo npm install -g browser-sync
 
 #INSTALLING ROS
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
@@ -98,13 +77,11 @@ rosdep update
 echo "source /opt/ros/kinetic/setup.bash" >> ~/.bashrc
 source ~/.bashrc
 sudo apt install python-rosinstall python-rosinstall-generator python-wstool build-essential
-
 #ROS ENVIRONMENT SETUP
 mkdir -p ~/catkin_ws/src
 cd ~/catkin_ws
 catkin_make
 source devel/source.bash
-
 #ROBOSUB DOWNLOAD
 cd src
 git clone https://github.com/auv-iitb/robosub.git
@@ -112,7 +89,6 @@ echo "source ~/catkin_ws/devel/setup.bash
 source ~/catkin_ws/src/robosub/auv_utils/src/utility/bash/robosub_bash
 source ~/catkin_ws/src/robosub/auv_utils/src/utility/bash/robosub_aliases" >> ~/.bashrc
 source ~/.bashrc
-
 #FFTW LIBRARY
 cd ~
 wget "http://www.fftw.org/fftw-3.3.8.tar.gz"
@@ -121,19 +97,15 @@ cd fftw-3.3.8
 sudo ./configure
 sudo make
 sudo make install
-
 #BFL LIBRARY
 sudo apt update
 sudo apt install ros-kinetic-bfl
-
 #PUGIXML LIBRARY
 sudo apt update
 sudo apt install libpcap0.8 libpcap0.8-dev libpcap-dev
-
 #PCAP LIBRARY
 sudo apt update
 sudo apt install libpugixml-dev libpugixml1v5 libpugixml1v5-dbgsym pugixml-doc
-
 #INSTALLING ROBOSUB
 cd ~/catkin_ws
 catkin_make
@@ -153,3 +125,25 @@ echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sou
 sudo apt update
 sudo apt install sublime-text
 sudo apt install sublime-merge
+
+#FOXIT-READER
+#cd /tmp
+#wget http://cdn01.foxitsoftware.com/pub/foxit/reader/desktop/linux/2.x/2.4/en_us/FoxitReader2.4.1.0609_Server_x64_enu_Setup.run.tar.gz
+#tar -xvf FoxitReader*.tar.gz
+#sudo ./FoxitReader.enu.setup.2.4.1.0609\(r08f07f8\).x64.run
+wget http://cdn01.foxitsoftware.com/pub/foxit/reader/desktop/linux/2.x/2.1/en_us/FoxitReader2.1.0805_Server_x64_enu_Setup.run.tar.gz
+tar xzvf FoxitReader*.tar.gz
+sudo chmod a+x FoxitReader*.run
+sudo ./FoxitReader.*.run
+
+#WPS-OFFICE (WORD, EXCEL, PRESENTATION)
+sudo snap install wps-office
+
+#PDFSAM (PDF EDITOR)
+sudo apt install openjdk-8-jre libopenjfx-jni libopenjfx-java openjfx
+sudo echo "JAVA_HOME=\"/usr/lib/jvm/java-8-openjdk-amd64\"" >> /etc/environment
+source /etc/environment
+echo $JAVA_HOME
+wget https://github.com/torakiki/pdfsam/releases/download/v3.1.0.RELEASE/pdfsam-3.1.0.RELEASE-1_all.deb -O pdfsam-3.1.0.RELEASE-1_all.deb
+sudo dpkg -i pdfsam-3.1.0.RELEASE-1_all.deb
+
