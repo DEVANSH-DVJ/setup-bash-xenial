7a6aa62294c45270ddb3b7f574ed3674474bd162
devansh-dvj <devansh.dvj@gmail.com>
2020-06-08 21:18:00 +0530

    08062020 2118

--- 3rd/Git&Python.sh
+++ 3rd/Git&Python.sh
@@ -37,21 +37,21 @@ virtualenv -p python2.7 2.7.12;
 
 source 2.7.12/bin/activate;
 pip install --upgrade pip;
-pip install setuptools==44.0.0 Django numpy scipy matplotlib ipython jupyter pandas astropy pygame;
+pip install setuptools==44.0.0 Django numpy scipy matplotlib ipython jupyter pandas astropy pygame wget lxml channels xlrd;
 deactivate;
 
 source 3.5.2/bin/activate;
 pip install --upgrade pip;
-pip install Django numpy scipy matplotlib ipython jupyter pandas astropy pygame;
+pip install Django numpy scipy matplotlib ipython jupyter pandas astropy pygame wget lxml xlrd;
 deactivate;
 
 source 3.8.2/bin/activate;
 pip install --upgrade pip;
-pip install Django numpy scipy matplotlib ipython jupyter pandas bs4 requests astropy pygame;
+pip install Django numpy scipy matplotlib ipython jupyter pandas wheel bs4 requests astropy wget lxml xlrd;
 deactivate;
 
-echo "alias 1='source ~/virtualenvs/2.7.12/bin/activate';
-alias 2='source ~/virtualenvs/3.5.2/bin/activate';
+echo "alias 2='source ~/virtualenvs/2.7.12/bin/activate';
+alias 5='source ~/virtualenvs/3.5.2/bin/activate';
 alias 3='source ~/virtualenvs/3.8.2/bin/activate';
 alias 0='deactivate';
 alias jp='source ~/virtualenvs/3.8.2/bin/activate; jupyter-notebook';
