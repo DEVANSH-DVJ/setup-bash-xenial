d1501fa1b13f8748cc1c956f6d1d05c298ca3906
Devansh Jain <devansh.dvj@gmail.com>
2020-08-07 23:57:57 +0530

    Added ROS installation script

--- big-installs/ros.sh
+++ big-installs/ros.sh
@@ -1,4 +1,4 @@
-udo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list';
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list';
 sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654;
 sudo apt update;
 sudo apt -y install ros-kinetic-desktop-full;
