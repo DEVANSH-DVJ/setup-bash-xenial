e1006b8fdf786b6e9ea32f70720ba60baa1287a5
devansh-dvj <devansh.dvj@gmail.com>
2020-08-09 21:15:19 +0530

    Fixed catkin_make issue

--- big-installs/ros.sh
+++ big-installs/ros.sh
@@ -6,5 +6,33 @@ sudo rosdep init;
 sudo rosdep update;
 sudo rosdep fix-permissions;
 echo "source /opt/ros/kinetic/setup.bash" >> ~/.bashrc;
-source ~/.bashrc;
 sudo apt -y install python-rosinstall python-rosinstall-generator python-wstool build-essential;
+source ~/.bashrc;
+
+mkdir -p ~/catkin_ws/src;
+cd ~/catkin_ws;
+catkin_make;
+source devel/setup.bash;
+
+cd src;
+git clone git@github.com:DEVANSH-DVJ/first_ros_task.git;
+git clone git@github.com:DEVANSH-DVJ/ros_beginner_tutorials.git;
+git clone git@github.com:auv-iitb/robosub.git;
+cd robosub;
+git checkout matsya_5_branch;
+echo "source ~/catkin_ws/devel/setup.bash;
+source ~/catkin_ws/src/robosub/auv_utils/src/utility/bash/robosub_bash;
+source ~/catkin_ws/src/robosub/auv_utils/src/utility/bash/robosub_aliases;
+alias wrapper.py='python ~/catkin_ws/src/robosub/auv_utils/src/utility/wrapper/wrapper.py';
+alias cm6='cm -j6';
+" >> ~/.bashrc;
+source ~/.bashrc;
+
+sudo apt -y install libfftw3-dev;
+sudo apt -y install ros-kinetic-bfl;
+sudo apt -y install libpcap-dev;
+sudo apt -y install libpugixml-dev;
+sudo apt -y install ros-kinetic-gazebo-ros-pkgs ros-kinetic-gazebo-ros-control;
+
+cd ~/catkin_ws;
+catkin_make;
